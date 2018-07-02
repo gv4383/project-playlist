3575f7f7492049caf2f4e3470894b9f607756eb0
Goyo Vargas <gv4383@Goyos-MBP.attlocal.net>
2018-07-01 22:15:29 -0500

    added dependencies and fixed problem with massive

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(json());
 app.use(cors());
 
 // Allows local server to utilize SQL commands within db folder
-app.use(massive(process.env.CONNECTION_STRING))
+massive(process.env.CONNECTION_STRING)
   .then(dbInstance => {
     app.set("db", dbInstance);
 }).catch(err => console.log(err));
@@ -32,4 +32,4 @@ app.use(
 
 // Runs the server on localhost:3001
 const port = process.env.PORT || 3001;
-app.listen(port, `Listening on port: ${ port }`);
+app.listen(port, () => { console.log(`Listening on port: ${ port }`) });
