c356e86746cea9dbd4b1e34b0da1ed9ffc4aa3cc
Goyo Vargas <gv4383@Goyos-MBP.attlocal.net>
2018-07-04 21:38:01 -0500

    implemented search track functionality

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import store from './redux/store';
 
 import logo from './logo.svg';
 import './App.css';
+import { EventEmitter } from 'events';
 
 const spotifyApi = new SpotifyWebApi();
 
@@ -28,7 +29,13 @@ class App extends Component {
         name: 'Not Checked',
         albumArt: '',
         spotifyUri: ''
-      }
+      },
+      playlistName: '',
+      playlistTracks: [],
+      trackUri0: '',
+      trackUri1: '',
+      trackUri2: '',
+      searchedSongInput: ''
     }
 
     console.log(params);
@@ -49,7 +56,7 @@ class App extends Component {
   getNowPlaying = () => {
     spotifyApi.getMyCurrentPlaybackState()
       .then((response) => {
-        console.log(response);
+        // console.log(response);
         this.setState({
           nowPlaying: {
             name: response.item.name,
@@ -57,12 +64,44 @@ class App extends Component {
             spotifyUri: response.item.uri
           }
         });
-      }).then(console.log(this.state));
+      });
+      // .then(console.log(this.state));
+  }
+
+  getAPlaylist = () => {
+    spotifyApi.getPlaylist() // put user's spotify id and playlist id as arguments here
+      .then((response) => {
+        // console.log(response);
+        this.setState({
+          playlistName: response.name,
+          playlistTracks: response.tracks.items,
+          trackUri0: response.tracks.items[0].track.uri,
+          trackUri1: response.tracks.items[1].track.uri,
+          trackUri2: response.tracks.items[4].track.uri
+        });
+      });
+  }
+
+  searchSong = (input) => {
+    spotifyApi.searchTracks(input)
+      .then((response) => {
+        console.log(response);
+      });
+  }
+
+  onChangeHandler = (event) => {
+    this.setState({
+      searchedSongInput: event.target.value
+    });
   }
 
   render() {
-    let embedUri = `https://open.spotify.com/embed?uri=${ this.state.nowPlaying.spotifyUri }`;
-    console.log(embedUri);
+    let baseUri = 'https://open.spotify.com/embed?uri=';
+    let embedUri = baseUri + this.state.nowPlaying.spotifyUri;
+    // console.log(embedUri);
+    let embedUri0 = baseUri + this.state.trackUri0;
+    let embedUri1 = baseUri + this.state.trackUri1;
+    let embedUri2 = baseUri + this.state.trackUri2;
 
     return (
       <Provider store={ store }>
@@ -89,6 +128,37 @@ class App extends Component {
                 <button onClick={ () => this.getNowPlaying() }>
                   Check Now Playing
                 </button>
+              }
+            </div>
+            <br />
+            <h3>Playlist Name: { this.state.playlistName }</h3>
+            <div>
+              <iframe src={ embedUri0 } width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            </div>
+            <div>
+              <iframe src={ embedUri1 } width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            </div>
+            <div>
+              <iframe src={ embedUri2 } width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            </div>
+            <div>
+              { this.state.loggedIn &&
+                <button onClick={ () => this.getAPlaylist() }>
+                  Get Playlist
+                </button>
+              }
+            </div>
+            <br />
+            <div>
+              { this.state.loggedIn &&
+                <div>
+                  <input placeholder="Search for a song" value={ this.state.searchedSongInput } onChange={ this.onChangeHandler } />
+                  <button onClick={ () => this.searchSong(this.state.searchedSongInput) }>
+                    Search Song
+                  </button>
+                  <br />
+                  <br />
+                </div>
               }
             </div>
             {/* { routes } */}
