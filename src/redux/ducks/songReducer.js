2e0d376a7eeb6c1acedc94660c82fa6d7dac0382
Goyo Vargas <gv4383@Goyos-MacBook-Pro.local>
2018-07-05 14:29:36 -0500

    reconfigured redux to take in access/refresh tokens

--- src/redux/ducks/songReducer.js
+++ src/redux/ducks/songReducer.js
@@ -4,11 +4,15 @@ import axios from 'axios';
 const initialState = {
   songs: [],
   isLoading: false,
-  error: ''
+  error: '',
+  accessToken: '',
+  refreshToken: ''
 }
 
 /****** ACTION TYPES ******/
 const GET_SONGS = 'GET_SONGS';
+const GET_ACCESS_TOKEN = 'GET_ACCESS_TOKEN';
+const GET_REFRESH_TOKEN = 'GET_REFRESH_TOKEN';
 
 // gets list of songs from server
 export function getSongs() {
@@ -16,6 +20,20 @@ export function getSongs() {
     type: GET_SONGS,
     payload: axios.get('/api/songs')
   };
+}
+
+export function getAccessToken(accessToken) {
+  return {
+    type: GET_ACCESS_TOKEN,
+    payload: accessToken
+  }
+}
+
+export function getRefreshToken(refreshToken) {
+  return {
+    type: GET_REFRESH_TOKEN,
+    payload: refreshToken
+  }
 }
 
 /****** REDUCER ******/
@@ -32,12 +50,24 @@ export default function songReducer(state = initialState, action) {
         ...state,
         isLoading: false,
         songs: action.payload.data
-      };
+    };
     case 'GET_SONGS_REJECTED':
       return {
         ...state,
         isLoading: true,
         error: action.payload
+    };
+
+    case 'GET_ACCESS_TOKEN':
+      return {
+        ...state,
+        accessToken: action.payload
+      };
+
+      case 'GET_REFRESH_TOKEN':
+        return {
+          ...state,
+          refreshToken: action.payload
       };
 
     // DEFAULT
