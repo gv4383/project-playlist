d2e2a8821925cacfa1207d455361c4510a1fb776
gv4383 <gregory.vargas18@gmail.com>
2018-07-21 13:29:33 -0500

    user page now displays the information of the current user

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi();
 
 class User extends Component {
   
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      country: '',
+      email: '',
+      username: '',
+      image: ''
+    }
+  }
+
+  componentDidMount = () => {
+    spotifyApi.getMe()
+      .then((response) => {
+        console.log('response: ', response);
+        this.setState({
+          country: response.country,
+          email: response.email,
+          username: response.id,
+          image: response.images[0].url
+        });
+      });
+  }
+  
   render() {
+    console.log('state: ', this.state);
+
+    const { country, email, username, image } = this.state;
 
     return (
       <div>
-        <h1 className="clear">User</h1>
+        <h1 className="clear">{ username }</h1>
         ( ͡° ͜ʖ ͡°)
+        <br />
+        <div>
+          <img src={ image } alt="" />
+          <h3>Email: { email }</h3>
+          <h3>Country: { country }</h3>
+        </div>
       </div>
     );
   }
